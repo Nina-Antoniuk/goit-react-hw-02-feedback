04b4926a6bba4b11f6ec113c8f74eff914aef31a
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-10-07 22:25:55 +0300

    Updates

---
+++ static/js/main.d8f413a9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),s=c.n(i),a=(c(8),c(9),c(0));var o=function(){return Object(a.jsx)("div",{className:"App",children:"test"})};s.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.d8f413a9.chunk.js.map
